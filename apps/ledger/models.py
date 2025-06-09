302fb3ae8a4f04398ccfbbd53db7e80cea7d5e8d
Oliver Mutuku <olivermulei001.com>
2025-06-09 12:35:29 +0300

    add model BudgetType for users to define their custom budget types

--- apps/ledger/models.py
+++ apps/ledger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify # to turn any string into a slugified string
 
 
 class Category(models.Model):
@@ -12,18 +13,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+
 class Expense(models.Model):
-    BUDGET_TYPE_CHOICES = [
-        ('need', 'Need'),
-        ('want', 'Want'),
-        ('savings', 'Savings/Investment'),
-    ]
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='expenses')
     title = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    budget_type = models.CharField(max_length=10, choices=BUDGET_TYPE_CHOICES, default='need')
+    budget_type = models.ForeignKey('BudgetType', on_delete=models.SET_NULL, null=True, to_field='slug', related_name='expenses')
     description = models.TextField(blank=True)
     date = models.DateField() # for filtering purposes
     created_at = models.DateTimeField(auto_now_add=True)
@@ -54,6 +51,24 @@ class Income(models.Model):
         return f"{self.source} - ksh{self.amount} on {self.date}"
 
 
+class BudgetType(models.Model):
+    slug = models.SlugField(primary_key=True, unique=True, max_length=50, blank=True)
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, related_name='budget_types')
+    description = models.TextField(blank=True)
 
+
+    class Meta:
+        unique_together = ('user', 'name')
+        verbose_name = 'budget type'
+        verbose_name_plural = 'budget types'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name} ({self.slug})"
 
 
