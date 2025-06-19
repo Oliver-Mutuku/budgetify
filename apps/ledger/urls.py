ca7d5f427de9ef9c8dff0fea4f218ddd79b7ca7e
jaycode8 <jamesmumo443@gmail.com>
2025-06-19 09:34:46 +0300

    fix(model) fixed user relation in expens and income models

--- apps/ledger/urls.py
+++ apps/ledger/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, TypesViewSet
+from .views import CategoryViewSet, TypesViewSet, ExpenseViewSet, IncomeViewSet
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'types', TypesViewSet, basename='budget_type')
-router.register(r'incomes', TypesViewSet, basename='income')
-router.register(r'expenses', TypesViewSet, basename='expense')
+router.register(r'incomes', IncomeViewSet, basename='income')
+router.register(r'expenses', ExpenseViewSet, basename='expense')
 urlpatterns =router.urls
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,9 +1,12 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(AbstractUser):
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     phone_number = PhoneNumberField(blank=True)
     middle_name = models.CharField(max_length=100, blank=True)
 
